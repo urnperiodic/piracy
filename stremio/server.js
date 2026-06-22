6718bf9938cb7d618bd188d3dd77a9ea86910d0f
Classroomgoogle <tharaniidaranthamizhmani@gmail.com>
2026-06-22 18:48:41 -0400

    Delete stremio directory

--- stremio/server.js
+++
@@ -1,9 +0,0 @@
-const { serveHTTP } = require("stremio-addon-sdk");
-const addonInterface = require("./addon");
-
-// This automatically handles CORS and the /manifest.json path
-serveHTTP(addonInterface, { 
-    port: process.env.PORT || 7000 
-});
-
-console.log("Addon is live and CORS is enabled.");
